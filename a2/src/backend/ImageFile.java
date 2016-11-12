acb31713181f69aa57c9bba8f29eb52e9de533fd
kevinyun <kevin.yun@mail.utoronto.ca>
2016-11-12 17:49:54 -0500

    Done, maybe missing few methods

--- a2/src/backend/ImageFile.java
+++ a2/src/backend/ImageFile.java
@@ -1,5 +1,45 @@
 package backend;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ImageFile {
+
+	String filePath;
+	String fileName;
+	HashMap<Date, String> nameHistory = new HashMap<>();
+	ArrayList<String> tags = new ArrayList<String>();
+
+	public ImageFile(String filePath, String fileName, HashMap<Date, String> nameHistory, ArrayList<String> tags) {
+		this.filePath = filePath;
+		this.fileName = fileName;
+		this.nameHistory = nameHistory;
+		this.tags = tags;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public HashMap<Date, String> getNameHistory() {
+		return nameHistory;
+	}
+
+	public ArrayList<String> getTags() {
+		return tags;
+	}
 
 }
--- a2/src/backend/Tag.java
+++ a2/src/backend/Tag.java
@@ -1,5 +1,36 @@
 package backend;
 
-public class Tag {
+import java.util.ArrayList;
 
+public class Tag {
+	ArrayList<String> tagList = new ArrayList<String>();
+
+	public Tag(ArrayList<String> tagList) {
+		this.tagList = tagList;
+	}
+
+	public ArrayList<String> getTagList() {
+		return tagList;
+	}
+
+	public void addTagList(String tag) {
+		// Check to see if the tag is already in the list of tags
+		if (this.tagList.contains(tag) == false) {
+			this.tagList.add(tag);
+		}
+	}
+
+	@Override
+	public String toString() {
+		String tags = "";
+		for (String tag : tagList) {
+			tags = tags + tag + ", ";
+		}
+		tags.trim();
+		return tags.substring(0, tags.length() - 1);
+	}
+
+	public int getCount() {
+		return this.tagList.size();
+	}
 }
