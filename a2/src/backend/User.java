318bcd93e49f288008bb357ad2cf025b45faac73
Vugar Mammadli <v-m13@windowslive.com>
2016-11-12 15:35:15 -0500

    Add tag is implemented.

--- a2/src/backend/User.java
+++ a2/src/backend/User.java
@@ -1,49 +1,59 @@
 package backend;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.*;
 
 import javax.activation.MimetypesFileTypeMap;
 
 public class User {
-	
-	public User(){
-		
+
+	public User() {
+
 	}
-	
+
 	/**
 	 * Returns list of all image files anywhere under the directory.
-	 * @param directory folder to search for image files
+	 * 
+	 * @param directory
+	 *            folder to search for image files
 	 * @return list of all image files
 	 */
-	public List<File> getAllImages(File directory){
+	public List<File> getAllImages(File directory) {
 		List<File> allImages = new ArrayList<>();
-		
-		for(File file : directory.listFiles()){
-			if(file.isFile()){
-				if(checkImageType(file))
+
+		for (File file : directory.listFiles()) {
+			if (file.isFile()) {
+				if (checkImageType(file))
 					allImages.add(file);
-			} else if (file.isDirectory()){
+			} else if (file.isDirectory()) {
 				allImages.addAll(getAllImages(file));
 			}
 		}
-		
+
 		return allImages;
 	}
 	
 	/**
+	 * Creates new tag with the name
+	 * @param name the name of new tag
+	 */
+	public void addTag(String name){
+		Tag.getAllTags().add(new Tag(name));
+	}
+
+	/**
 	 * Helper function to check if the file type is image or not.
-	 * @param file the file to be checked
+	 * 
+	 * @param file
+	 *            the file to be checked
 	 * @return true if the file is image
 	 */
-	private boolean checkImageType(File file){
+	private boolean checkImageType(File file) {
 		String mimetype = new MimetypesFileTypeMap().getContentType(file);
-		
-		if(mimetype != null && mimetype.split("/")[0].equals("image"))
+
+		if (mimetype != null && mimetype.split("/")[0].equals("image"))
 			return true;
-		
+
 		return false;
 	}
 }
