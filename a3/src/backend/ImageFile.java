025c38315a0f1d03696db4d821e14ce80b0c309d
Vugar Mammadli <v-m13@windowslive.com>
2016-11-26 17:33:59 -0500

    removed files readded to the project.

---
+++ a3/src/backend/ImageFile.java
@@ -0,0 +1,228 @@
+package backend;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.logging.*;
+
+public class ImageFile implements Serializable {
+	private static final long serialVersionUID = 4147923517015278579L;
+	private File file;
+	private String originalName;
+	private Map<Date, String> nameHistory;
+	private List<Tag> tags;
+	private static List<ImageFile> allImageFiles = new ArrayList<ImageFile>();
+
+	private static final Logger logger = Logger.getLogger(ImageFile.class.getName());
+	private static FileHandler simpleHandler;
+
+	public ImageFile(File file) {
+		this.file = file;
+		this.originalName = file.getName();
+		this.nameHistory = new HashMap<Date, String>();
+		nameHistory.put(new Date(), file.getName());
+		this.tags = new ArrayList<Tag>();
+
+		try {
+			simpleHandler = new FileHandler("src/data/log.txt", true);
+		} catch (SecurityException | IOException e) {
+			e.printStackTrace();
+		}
+
+		simpleHandler.setFormatter(new LogFormatter());
+		logger.addHandler(simpleHandler);
+	}
+
+	/**
+	 * Returns nameHistory in a friendly format
+	 * 
+	 * @return string representation of nameHistory
+	 */
+	public String getNameHistory() {
+		String res = "";
+		SimpleDateFormat format = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+		for (Date date : this.nameHistory.keySet()) {
+			res += "Date: " + format.format(date);
+			res += " Name: " + this.nameHistory.get(date) + "\n";
+		}
+		return res;
+	}
+
+	/**
+	 * Returns originalName
+	 * 
+	 * @return the original name of this image file
+	 */
+	public String getOriginalName() {
+		return this.originalName;
+	}
+
+	/**
+	 * Returns tags
+	 * 
+	 * @return the list of tags for this image file
+	 */
+	public List<Tag> getTags() {
+		return this.tags;
+	}
+	
+	/**
+	 * Returns tag of this image by name.
+	 * @param tagName Name of tag to return
+	 * @return tag iff there is a tag with tagName.
+	 */
+	public Tag getTagOfImage(String tagName){
+		for (Tag t : this.getTags()) {
+			if (t.getName().equals(tagName))
+				return t;
+		}
+		return null;
+	}
+
+	/**
+	 * Return allImageFiles
+	 * 
+	 * @return the list of all image files
+	 */
+	public static List<ImageFile> getAllImageFiles() {
+		return allImageFiles;
+	}
+
+	/**
+	 * Sets the value of allImageFiles
+	 * 
+	 * @param allImageFiles new list of image files
+	 */
+	public static void setAllImageFiles(List<ImageFile> allImageFiles) {
+		ImageFile.allImageFiles = allImageFiles;
+	}
+
+	/**
+	 * Adds tag to this image file. Renames the file
+	 * 
+	 * @param tags new set of tags to be added for this image file
+	 */
+	public void addTag(List<Tag> tags) {
+		for (Tag tag : tags) {
+			if (!this.tags.contains(tag)) {
+				this.tags.add(tag);
+				tag.setNumUsed(tag.getNumUsed() + 1);
+			}
+		}
+		renameFile();
+		try {
+			Configuration.saveImageFiles(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Removes tag from this image file
+	 * 
+	 * @param tag new tag to be removed for this image file
+	 */
+	public void removeTag(Tag tag) {
+		if (this.tags.contains(tag)) {
+			this.tags.remove(tag);
+			tag.setNumUsed(tag.getNumUsed() - 1);
+		}
+
+		renameFile();
+
+		try {
+			Configuration.saveImageFiles(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Reverts to a past name
+	 * 
+	 * @param name past name to revert to
+	 */
+	public void changeName(String name) {
+		if (this.nameHistory.values().contains(name)) {
+			SimpleDateFormat format = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+			logger.log(Level.INFO,
+					"Date: " + format.format(new Date()) + " Old name: " + this.getName() + " New name: " + name);
+			simpleHandler.close();
+			String path = this.file.getAbsolutePath().split(this.getName())[0];
+			File newFile = new File(path + name);
+			this.file.renameTo(newFile);
+			this.file = newFile;
+			this.nameHistory.put(new Date(), name);
+		}
+	}
+
+	/**
+	 * Helper function to change file name according to tags
+	 */
+	private void renameFile() {
+		String fileName = this.originalName;
+
+		// creates new file name with previous one and new tags
+		String newFileName = fileName.split("\\.")[0];
+		for (Tag tag : this.tags) {
+			newFileName += " @" + tag.getName();
+		}
+		newFileName += "." + fileName.split("\\.")[1];
+
+		// path of current file
+		String path = this.file.getAbsolutePath().split(this.getName())[0];
+
+		SimpleDateFormat format = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+		logger.log(Level.INFO,
+				"Date: " + format.format(new Date()) + " Old name: " + this.getName() + " New name: " + newFileName);
+		simpleHandler.close();
+		// changes file name
+		File newFile = new File(path + newFileName);
+		this.file.renameTo(newFile);
+		this.file = newFile;
+
+		this.nameHistory.put(new Date(), newFileName);
+	}
+
+	/**
+	 * Returns current name
+	 * 
+	 * @return the current name of ImageFile
+	 */
+	public String getName() {
+		List<Date> allNameDates = new ArrayList<Date>(this.nameHistory.keySet());
+		Collections.sort(allNameDates);
+		Date lastDate = allNameDates.get(allNameDates.size() - 1);
+
+		return this.nameHistory.get(lastDate);
+	}
+
+	@Override
+	/**
+	 * Returns string representation of this image file
+	 * 
+	 * @return string representation of image file
+	 */
+	public String toString() {
+		return "Image: " + this.getName();
+	}
+
+	@Override
+	/**
+	 * Returns true if two images files are equal according to name
+	 * 
+	 * @param other ImageFile object to compare to
+	 * 
+	 * @return true two ImageFile are same
+	 */
+	public boolean equals(Object other) {
+		if (other == null || !(other instanceof ImageFile))
+			return false;
+		if (other == this)
+			return true;
+
+		return (((ImageFile) other).file == this.file && ((ImageFile) other).getName().equals(this.getName()));
+	}
+}
---
+++ a3/src/backend/User.java
@@ -0,0 +1,188 @@
+package backend;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import javax.activation.MimetypesFileTypeMap;
+
+public class User {
+	/**
+	 * The class is in singleton pattern. Since the user only person who is the
+	 * owner of the computer, only he can use the application to tag photos in
+	 * their computer. So only one user is needed to coordinate actions across
+	 * the app.
+	 */
+
+	private static final User instance = new User();
+
+	private User() {
+	}
+
+	public static User getInstance() {
+		return instance;
+	}
+
+	/**
+	 * Returns list of all image files anywhere under the directory.
+	 * 
+	 * @param directory
+	 *            folder to search for image files
+	 * @return list of all image files
+	 */
+	public List<ImageFile> getAllImages(File directory) {
+		List<ImageFile> allImages = new ArrayList<>();
+
+		for (File file : directory.listFiles()) {
+			if (file.isFile()) {
+				if (checkImageType(file)) {
+					allImages.add(new ImageFile(file));
+				}
+			} else if (file.isDirectory()) {
+				allImages.addAll(getAllImages(file));
+			}
+		}
+
+		return allImages;
+	}
+
+	/**
+	 * Creates new tag with the name
+	 * 
+	 * @param name
+	 *            the name of new tag
+	 * @return true if the tag is not already exists and created successfully
+	 */
+	public boolean addTag(String name) {
+		if (name.isEmpty() || name == null)
+			return false;
+
+		Tag newTag = new Tag(name);
+
+		if (Tag.getAllTags().contains(newTag))
+			return false;
+
+		Tag.getAllTags().add(newTag);
+		return true;
+	}
+
+	/**
+	 * Returns tag with the name
+	 * 
+	 * @param name
+	 *            the name of the searched tag
+	 * @return Tag if there is a tag with the name, otherwise null.
+	 */
+	public Tag getTag(String name) {
+		for (Tag t : Tag.getAllTags()) {
+			if (t.getName().equals(name))
+				return t;
+		}
+		return null;
+	}
+
+	/**
+	 * Adds selected tags to the file.
+	 * 
+	 * @param file
+	 *            the image to tag
+	 * @param tags
+	 *            the tags to add image file
+	 * @return true if image tagged successfully
+	 */
+	public boolean selectTag(ImageFile file, List<Tag> tags) {
+		if (tags != null && file != null) {
+			for (Tag t : tags) {
+				if (!Tag.getAllTags().contains(t))
+					return false;
+			}
+			file.addTag(tags);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Removes tag from the list of all tags.
+	 * 
+	 * @param tag
+	 *            the tag that should be removed
+	 * @return true iff there is a tag and it is deleted successfully.
+	 */
+	public boolean deleteTag(Tag tag) {
+		if (!Tag.getAllTags().contains(tag))
+			return false;
+
+		Tag.getAllTags().remove(tag);
+		return true;
+	}
+
+	/**
+	 * Remove tag from the selected image
+	 * 
+	 * @param selectedImage
+	 *            the image to remove tag
+	 * @param tag
+	 *            the tag to remove from image
+	 * @return true iff the tag removed from image successfully
+	 */
+	public boolean deleteTagFromImage(ImageFile selectedImage, Tag tag) {
+		if (selectedImage != null && tag != null) {
+			if (selectedImage.getTags().contains(tag)) {
+				selectedImage.removeTag(tag);
+				return true;
+			} else
+				return false;
+		} else
+			return false;
+	}
+
+	/**
+	 * Reverts the name of the file to selected name
+	 * 
+	 * @param file
+	 *            the file that is changed
+	 * @param name
+	 *            the name to revert back
+	 */
+	public void revertName(ImageFile file, String name) {
+		if (file != null && !file.getName().equals(name)) {
+			file.changeName(name);
+		}
+	}
+
+	/**
+	 * Returns the most common tag.
+	 * 
+	 * @return the most common tag that files tagged.
+	 */
+	public Tag getMostCommonTag() {
+		if (!Tag.getAllTags().isEmpty()) {
+			Tag tag = Tag.getAllTags().get(0);
+			for (Tag t : Tag.getAllTags()) {
+				if (t.getNumUsed() > tag.getNumUsed())
+					tag = t;
+			}
+			return tag;
+		}
+		return null;
+	}
+
+	/**
+	 * Helper function to check if the file type is image or not. Source:
+	 * http://stackoverflow.com/questions/9643228/test-if-file-is-an-image
+	 * 
+	 * @param file
+	 *            the file to be checked
+	 * @return true if the file is image
+	 */
+	private boolean checkImageType(File file) {
+		String mimetype = new MimetypesFileTypeMap().getContentType(file);
+
+		if (mimetype != null && mimetype.split("/")[0].equals("image"))
+			return true;
+
+		return false;
+	}
+}
